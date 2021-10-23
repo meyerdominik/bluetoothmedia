6c1720a36efcf1cd347b537c823f8a71f2bd8933
Dominik <mail@meyerdominik.de>
2021-10-23 17:15:00 +0200

    Cleanup

--- bluetooth_media.py
+++ bluetooth_media.py
@@ -4,11 +4,12 @@ import can
 import dbus
 import dbus.mainloop.glib
 import sys
-#from gi.repository import GLib
+import bluetoothctl
 
 # Variablen zum aendern
 nCanBitrate = 100000
 sConnectedToCarCan = 'can0' # oder can1
+sBluetoothMACVonMeinemHandy = 'AC:D6:18:1C:ED:4C'
 
 # NICHT ZUM AENDERN
 # Vars
@@ -20,22 +21,41 @@ oBus = None
 oBluez = None
 oObjMgr = None
 
-# init dbus
-dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
-oBus = dbus.SystemBus()
-oBlueZ = oBus.get_object('org.bluez', "/")
-oObjMgr = dbus.Interface(oBlueZ, 'org.freedesktop.DBus.ObjectManager')
+bMeinHandyWarImRadius = False
+sLetzteMac = ''
+bBluetoothConnected = False
 
-# canbus init
-os.system('sudo ip link set can0 type can bitrate ' + str(nCanBitrate))
-os.system('sudo ifconfig can0 up')
+# CAN
+def CanInit():
+  os.system('sudo ip link set can0 type can bitrate ' + str(nCanBitrate))
+  os.system('sudo ifconfig can0 up')
+  os.system('sudo ip link set can1 type can bitrate ' + str(nCanBitrate))
+  os.system('sudo ifconfig can1 up')
+  oCan = can.interface.Bus(channel = sConnectedToCarCan, bustyp = 'socketcan_ctypes')
 
-os.system('sudo ip link set can1 type can bitrate ' + str(nCanBitrate))
-os.system('sudo ifconfig can1 up')
+def CanDeInit():
+  os.system('sudo ifconfig can0 down')
+  os.system('sudo ifconfig can1 down')
+  oCan = None
 
-oCan = can.interface.Bus(channel = sConnectedToCarCan, bustyp = 'socketcan_ctypes')
+def GetCanMessage():
+  if not oCan:
+    return None
+  else:
+    return oCan.recv()
 
-while True:
+# MediaControls
+def DbusInit():
+  dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+  oBus = dbus.SystemBus()
+  oBlueZ = oBus.get_object('org.bluez', "/")
+  oObjMgr = dbus.Interface(oBlueZ, 'org.freedesktop.DBus.ObjectManager')
+
+def MediaControlsExists():
+  if not oObjMgr:
+    # ObjectManager ist noch da
+    return False
+
   for path, ifaces in oObjMgr.GetManagedObjects().items():
     if 'org.bluez.MediaPlayer1' in ifaces:
       oMediaPlayerInterface = dbus.Interface(oBus.get_object('org.bluez', path), 'org.bluez.MediaPlayer1')
@@ -43,7 +63,10 @@ while True:
       oTransportInterface = dbus.Interface(oBus.get_object('org.bluez', path), 'org.freedesktop.DBus.Properties')
 
   if not oMediaPlayerInterface or not oTransportInterface:
-    # Wenn eins nicht vorhanden ist, wird abgebrochen
-    continue
+    return False
+  else:
+    return True
 
-  msg = oCan.recv()
+while True:
+  print 'Test'
+  break
